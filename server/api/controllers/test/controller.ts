a1806535db3ce35b94cd69e2d03fe2310b0a6e00
Eric Cheng <eric@concept-eng.co.nz>
2021-08-28 18:15:12 +1200

    create path for test

---
+++ server/api/controllers/test/controller.ts
@@ -0,0 +1,72 @@
+import ExamplesService from '../../services/examples.service';
+import { Request, Response } from 'express';
+
+export class Controller {
+  all(_: Request, res: Response): void {
+    res.json({
+      test: '123'
+    });
+  }
+
+  // byId(req: Request, res: Response): void {
+  //   const id = Number.parseInt(req.params['id']);
+  //   ExamplesService.byId(id).then((r) => {
+  //     if (r) res.json(r);
+  //     else res.status(404).end();
+  //   });
+  // }
+
+  create(_: Request, res: Response): void {
+    res.status(500).json({
+      abc: 123456,
+      def: "abcdefgh"
+    })
+  }
+
+  // delete(req: Request, res: Response): void {
+  //   const id = Number.parseInt(req.params['id']);
+  //   ExamplesService.delete(id).then((r) => {
+  //     if (r) res.json(r);
+  //     else res.status(404).end();
+  //   });
+  // }
+
+  // // if exists then update, else create new
+  // put(req: Request, res: Response): void {
+  //   const id = Number.parseInt(req.params['id']);
+  //   const name = req.body?.name;
+
+  //   // get the record
+  //   ExamplesService.byId(id).then((r) => {
+  //     if (!r) {
+  //       // create the record
+  //       if (!req.body?.id || req.body.id !== id) req.body.id = id; // assign the id if
+  //       ExamplesService.create(req.body.name).then((r) =>
+  //           res.status(201).location(`/api/v1/examples/${r.id}`).json(r)
+  //       );  
+  //     } else {
+  //       // update the record
+  //       r.name = name;
+  //       res.json(r);
+  //     }
+  //   });
+  // }
+
+  // // if exists then update, else create new
+  // patch(req: Request, res: Response): void {
+  //   const id = Number.parseInt(req.params['id']);
+  //   const name = req.body?.name;
+
+  //   // get the record
+  //   ExamplesService.byId(id).then((r) => {
+  //     if (!r) {
+  //       res.status(404).end();          
+  //     } else {
+  //       // update the record
+  //       r.name = name;
+  //       res.json(r);
+  //     }
+  //   });
+  // }
+}
+export default new Controller();
---
+++ server/api/controllers/test/router.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import controller from './controller';
+
+const router = express.Router();
+
+//////////////////////////////////////////////////////////////////
+// Routes
+//////////////////////////////////////////////////////////////////
+// router.route('/:id')
+//   .get(controller.byId)
+//   .delete(controller.delete)
+//   .put(controller.put)
+//   .patch(controller.patch)
+//   ;
+
+router.route('/')
+  .get(controller.all)
+  .post(controller.create)
+  ;
+
+
+export default router;
